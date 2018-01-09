abea402331b95774300718eab7d4866eabaacaa6
supermoe <gdmaxis@gmail.com>
2018-01-09 09:10:14 +0100

    Added main files.

---
+++ js/data.js
@@ -0,0 +1,182 @@
+die = ["d4", "d6", "d8", "d10", "d12"];
+benny_name = "Undo";
+attributes = ["Agility", "Smarts", "Spirit", "Strength", "Vigor"];
+levelUpCost = 5;
+paceDefault = 6;
+parryDefault = 2;
+toughnessDefault = 2;
+charismaDefault = 0;
+
+skills = [
+    //AGILITY
+    {
+        attribute:0,
+        name:'Security',
+        description:"The ability to set or, bypass securities such as locks and traps."
+    },
+    {
+        attribute:0,
+        name:'Shooting',
+        description:"One's effectiveness with ranged weapons."
+    },
+    {
+        attribute:0,
+        name:'Stealth',
+        description:"The faculty to remain unnoticed."
+    },
+    //SMARTS
+    {
+        attribute:1,
+        name:'Piloting',
+        description:"The ability to control vehicles of all kinds."
+    },
+    {
+        attribute:1,
+        name:'Gambling',
+        description:"The capacity to make succesful bets."
+    },
+    {
+        attribute:1,
+        name:'Healing',
+        description:"The faculty to mend wounds and cure diseases."
+    },
+    {
+        attribute:1,
+        name:'Knowledge',
+        description:"Wisdom and education."
+    },
+    {
+        attribute:1,
+        name:'Tinkering',
+        description:"One's ability to craft, modify or repair."
+    },
+    //SPIRIT
+    {
+        attribute:2,
+        name:'Coercion',
+        description:"The capacity to influence."
+    },
+    {
+        attribute:2,
+        name:'Instinct',
+        description:"The surreal feeling of good or bad."
+    },
+    {
+        attribute:2,
+        name:'Companionship',
+        description:"The capacity to help friends."
+    },
+    {
+        attribute:2,
+        name:'Awareness',
+        description:"The aptitude to notice details and to be left unsurprised."
+    },
+    //STRENGTH
+    {
+        attribute:3,
+        name:'Melee',
+        description:"One's talent with melee weapons."
+    },
+    {
+        attribute:3,
+        name:'Athletics',
+        description:"One's prowess in athletics and acrobatics."
+    }
+];
+
+MINOR = 1;
+MAJOR = 2;
+hindrances = [
+    {
+		name:"AllThumbs", type:MAJOR,
+		description:"-2 " + skills[7].name + "."
+	},
+	{
+		name:"Arrogant", type:MAJOR,
+		description:"Defy leadership, humiliate opponents."
+	},
+	{
+		name:"Bad Eyes", type:MINOR,
+		description:"-2 to Hit rolls when targeting distant opponents."
+	},
+	{
+		name:"Blind", type:MAJOR,
+		description:"-6 to rolls that rely on vision. +1 Edge."
+	},
+	{
+		name:"Bad Luck", type:MAJOR,
+		description:"-1 " + benny_name + " per session."
+	},
+	{
+		name:"Big Mouth", type:MINOR,
+		description:"Can't keep secrets. Makes inappropriate remarks."
+	},
+	{
+		name:"Bloodthirsty", type:MAJOR,
+		description:"Cannot useless than lethal force. Doesn't take prisoners."
+	},
+	{
+		name:"Careful", type:MINOR,
+		description:"Overly cautious."
+	},
+	{
+		name:"Coward", type:MINOR,
+		description:"-2 to Fear rolls."
+	},
+	{
+		name:"Honor", type:MAJOR,
+		description:"Can never lie or betray."
+	},
+	{
+		name:"Death Wish", type:MINOR,
+		description:"Finds ways to endanger themselves."
+	},
+	{
+		name:"Elderly", type:MAJOR,
+		description:"-1 Pace, -2 " + attributes[3] + ". -2 " + attributes[4] + ". +3 Skill Points."
+	},
+	{
+		name:"Greedy", type:MAJOR,
+		description:"Cannot give away items or money. Driven by material needs."
+	},
+	{
+		name:"Hard of hearing", type:MINOR,
+		description:"-2 " + skills[11].name + " when sounds are involved. Miss parts of conversations."
+	},
+	{
+		name:"Illiterate", type:MINOR,
+		description:"Cannot read or write."
+	},
+	{
+		name:"Loyal", type:MINOR,
+		description:"Protects friends."
+	},
+	{
+		name:"Obese", type:MINOR,
+		description:"+1 Toughness. -1 Pace. -2 " + skills[13].name + "."
+	},
+	{
+		name:"One eyed", type:MAJOR,
+		description:"-1 Charisma. -2 " + skills[1].name + "."
+	},
+	{
+		name:"Pacifist", type:MINOR,
+		description:"Can only fight in self defense."
+	},
+	{
+		name:"Poor", type:MINOR,
+		description:"No starting funds."
+	},
+	{
+		name:"Small", type:MINOR,
+		description:"-1 Toughness."
+	},
+	{
+		name:"Ugly", type:MINOR,
+		description:"-2 Charisma."
+	},
+	{
+		name:"Wanted", type:MINOR,
+		description:"Widely known criminal background."
+	}
+]
---
+++ js/main.js
@@ -0,0 +1,246 @@
+character = {
+	firstname: "unnamed",
+	exp: 0,
+	level: 0,
+	wounds: 0,
+	//attributes
+    attributes: [0,0,0,0,0],
+	attributePoints: 5,
+	//skills
+	skills: [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+	skillPoints: 10,
+	//hindrances
+	hindrances: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+	//edges
+	edgesPoints: 1
+}
+console.log(character.hindrances)
+attributeLabels = [];
+skillLabels = [];
+skillRows = [];
+hindranceRows = [];
+
+tempAttributes = {available:0, affected:[0,0,0,0,0]};
+tempAttributeLabels = [];
+
+tempSkills = {available:0, affected:[0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+tempSkillLabels = [];
+skillCostLabels = [];
+
+function update(){
+	//exp
+	$("#exp-label").text(character.exp);
+
+	//hide or show level up button
+	if (character.exp >= levelUpCost) $('#levelup-b').removeClass("hidden");
+	else $('#levelup-b').addClass("hidden");
+
+	//pace, parry and toughness
+	$("#pace-l").html(paceDefault);
+	var f = 0;
+	if (character.skills[12] >= 0) f = Math.floor((character.skills[12]*2+4)/2);
+	$("#parry-l").html(parryDefault + f);
+	$("#toughness-l").html(toughnessDefault + Math.floor((character.attributes[4]*2+4)/2));
+
+	//wounds
+	for (let i=0; i<5; i++) $("#wound"+i).removeClass("checked-wound");
+	$("#wound"+character.wounds).addClass("checked-wound");
+
+
+    for (var i=0; i<attributes.length; i++){
+        $(attributeLabels[i]).html(die[character.attributes[i]])
+    }
+    for (var i=0; i<skills.length; i++){
+        if (character.skills[i] < 0) {
+            $(skillLabels[i]).html("X");
+            $(skillRows[i]).addClass('hidden');
+        }
+        else {
+            $(skillLabels[i]).html(die[character.skills[i]]);
+            $(skillRows[i]).removeClass('hidden');
+        }
+    }
+	for (var i=0; i<hindrances.length; i++){
+		if (character.hindrances[i]){
+			$(hindranceRows[i]).removeClass('hidden');
+		}
+		else{
+			$(hindranceRows[i]).addClass('hidden');
+		}
+	}
+}
+
+function wound(value){
+	character.wounds = value;
+	update();
+};
+
+function skillDie(value){
+	if (value < 0) return "X";
+	else return die[value];
+}
+
+function costOfBump(i, nextValue){
+	if (nextValue < 1) return 2
+	if (nextValue > character.attributes[skills[i].attribute]) return 2
+	else return 1
+}
+
+function levelUp(){
+	character.level++;
+	character.exp -= levelUpCost;
+}
+
+//start
+
+for (let i=0; i<attributes.length; i++){
+    $("#attribute-brow").before("<tr><td>" + attributes[i] + ":" + "</td><td id='attribute-l" + i + "'></td></tr>");
+    attributeLabels.push('#attribute-l' + i);
+
+	$("#attribute-selector-brow").before("<tr><td>" + attributes[i] + "</td><td>" + die[character.attributes[i]] + " -> <span id='temp-attribute-l" + i + "'></span></td><td><div id='attribute-bump" + i + "' class='button'>+</div></td></tr>");
+	tempAttributeLabels.push('#temp-attribute-l' + i);
+
+	$("#attribute-bump" + i).click(function(){
+		if (character.attributes[i] + tempAttributes.affected[i] < die.length-1 && tempAttributes.available > 0){
+			tempAttributes.affected[i]++;
+			tempAttributes.available--;
+			updateAttributeSelector();
+		}
+	})
+}
+
+for (let i=0; i<skills.length; i++){
+    $("#skill-brow").before("<tr id='skill-row" + i + "'><td>" + skills[i].name + ":" + "</td><td id='skill-l" + i + "'></td></tr>");
+    skillLabels.push('#skill-l' + i);
+	skillRows.push('#skill-row' + i);
+	
+	$("#skill-selector-brow").before("<tr><td>" + skills[i].name + "</td><td>" + skillDie(character.skills[i]) + " -> <span id='temp-skill-l" + i + "'></span></td><td><span id='skill-cost" + i + "'></span></td><td><div id='skill-bump" + i + "' class='button'>+</div></td></tr>");
+	tempSkillLabels.push('#temp-skill-l' + i);
+	skillCostLabels.push('#skill-cost' + i);
+
+	$("#skill-bump" + i).click(function(){
+		var cost = costOfBump(i, character.skills[i] + tempSkills.affected[i]+1)
+		if (character.skills[i] + tempSkills.affected[i] < die.length-1 && tempSkills.available > cost){
+			tempSkills.affected[i]++;
+			tempSkills.available -= cost;
+			updateSkillSelector();
+		}
+	})
+}
+
+// Attribute selector --------------------------------------------
+function openAttributeSelector(){
+	$("#popup").removeClass("hidden")
+	$("#attribute-selector").removeClass("hidden")
+	attributeSelectorReset();
+}
+function closeAttributeSelector(){
+	$("#popup").addClass("hidden")
+	$("#attribute-selector").addClass("hidden")
+}
+function updateAttributeSelector(){
+	for (let i=0; i<attributes.length; i++){
+        $(tempAttributeLabels[i]).html(die[character.attributes[i]+tempAttributes.affected[i]]);
+    }
+	$("#attribute-selector-available").html(tempAttributes.available);
+}
+function attributeSelectorReset(){
+	for (let i=0; i<attributes.length; i++){
+		tempAttributes.affected[i] = 0;
+	}
+	tempAttributes.available = character.attributePoints;
+	updateAttributeSelector();
+}
+$("#attribute-selector-confirm-b").click(function(){
+	character.attributePoints = tempAttributes.available;
+	for (let i=0; i<attributes.length; i++){
+		character.attributes[i] = tempAttributes.affected[i] + character.attributes[i];
+	}
+	attributeSelectorReset();
+	closeAttributeSelector();
+	update();
+})
+
+// Skill selector -----------------------------------------------
+function openSkillSelector(){
+	$("#popup").removeClass("hidden")
+	$("#skill-selector").removeClass("hidden")
+	skillSelectorReset();
+}
+function closeSkillSelector(){
+	$("#popup").addClass("hidden")
+	$("#skill-selector").addClass("hidden")
+}
+function updateSkillSelector(){
+	for (let i=0; i<skills.length; i++){
+		$(tempSkillLabels[i]).html(skillDie(character.skills[i]+tempSkills.affected[i]));
+		$(skillCostLabels[i]).html(costOfBump(i, character.skills[i]+tempSkills.affected[i]+1));
+    }
+	$("#skill-selector-available").html(tempSkills.available);
+}
+function skillSelectorReset(){
+	for (let i=0; i<skills.length; i++){
+		tempSkills.affected[i] = 0;
+	}
+	tempSkills.available = character.skillPoints;
+	updateSkillSelector();
+}
+$("#skill-selector-confirm-b").click(function(){
+	character.skillPoints = tempSkills.available;
+	for (let i=0; i<skills.length; i++){
+		character.skills[i] = tempSkills.affected[i] + character.skills[i];
+	}
+	skillSelectorReset();
+	closeSkillSelector();
+	update();
+})
+
+// Level up window -----------------------------------------------
+function openLevelUp(){
+	$("#popup").removeClass("hidden")
+	$("#level-up").removeClass("hidden")
+}
+function closeLevelUp(){
+	$("#popup").addClass("hidden")
+	$("#level-up").addClass("hidden")
+}
+function edgeAdvance(){
+	character.edgesPoints++;
+	levelUp();
+	update();
+	closeLevelUp();
+}
+function skillAdvance(){
+	character.skillPoints += 2;
+	levelUp();
+	update();
+	closeLevelUp();
+}
+function attributeAdvance(){
+	character.attributePoints++;
+	levelUp();
+	update();
+	closeLevelUp();
+}
+
+// Exp selector
+function openXPSelector(){
+	$("#popup").removeClass("hidden")
+	$("#xp-selector").removeClass("hidden")
+}
+function closeXPSelector(){
+	$("#popup").addClass("hidden")
+	$("#xp-selector").addClass("hidden")
+}
+function addXPSelector(){
+	character.exp += parseInt($("#xp-input").val());
+	update();
+	closeXPSelector();
+}
+
+for (let i=0; i<hindrances.length; i++){
+    $("#hindrance-brow").before("<tr id='hindrance-row" + i + "'><td>" + hindrances[i].name + "<div class=hindrance-desc>" + hindrances[i].description + "</div></td></tr>");
+    hindranceRows.push('#hindrance-row' + i);
+}
+
+update();
