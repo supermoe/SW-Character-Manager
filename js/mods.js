8b6c07c1387ec00669572aa37ab4892830896169
supermoe <gdmaxis@gmail.com>
2018-01-17 00:24:37 +0100

    New mods

--- js/mods.js
+++ js/mods.js
@@ -5,6 +5,7 @@ SHARP_BLADE = {
 	id : "sharp_blade",
 	name: "Sharp Blade",
 	type: type.weapon,
+	removeable: true,
 	levels: [
 		{description: "+1 Damage against enemies with no armor.",},
 		{description: "+2 Damage against enemies with no armor.",},
@@ -17,6 +18,7 @@ ERGONOMIC = {
 	id : "ergonomic",
 	name: "Ergonomic",
 	type: type.weapon,
+	removeable: true,
 	levels: [
 		{description: "No penalty for using in offhand",},
 	]
@@ -27,6 +29,7 @@ HEAVY_ARMOR = {
 	id : "heavy_armor",
 	name: "Heavy Armor",
 	type: type.armor,
+	removeable: false,
 	levels: [
 		{description: "-1 Pace.",},
 		{description: "-2 Pace.",},
@@ -38,6 +41,7 @@ LIGHT_ARMOR = {
 	id : "light_armor",
 	name: "Light Armor",
 	type: type.armor,
+	removeable: true,
 	levels: [
 		{description: "+1 Pace.",},
 		{description: "+2 Pace.",},
@@ -49,6 +53,7 @@ BULLETPROOF = {
 	id : "bulletproof",
 	name: "Bulletproof",
 	type: type.armor,
+	removeable: true,
 	levels: [
 		{description: "+1 toughness when hit by ranged weapons.",},
 	]
@@ -56,11 +61,24 @@ BULLETPROOF = {
 mods.push(BULLETPROOF);
 
 CONCEALING = {
-	id : "CONCEALING",
+	id : "concealing",
 	name: "Concealing",
 	type: type.armor,
+	removeable: true,
 	levels: [
-		{description: "Makes it easy to hide weapons.",},
+		{description: "Makes it easy to hide small items",},
+		{description: "Makes it easy to hide items and weapons",},
+	]
+}
+mods.push(CONCEALING);
+
+UNCOMPROMISING = {
+	id : "uncompromising",
+	name: "Uncompomising",
+	type: type.weapon,
+	removeable: true,
+	levels: [
+		{description: "Reroll damage once if you got less than half your attack skill die.",},
 	]
 }
 mods.push(CONCEALING);
